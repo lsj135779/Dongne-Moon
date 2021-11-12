55734dee3b28b57f240b6a4485257c307a667386
Lawen-s <bg920611@gmail.com>
2021-11-12 14:57:35 +0900

    server:signin:add:add accesstoken, router

---
+++ server/controllers/user/userCtrl.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // modifyImg: require("./modifyImg"),
+    // modifyNickname: require("./modifyNickname"),
+    // modifyIntro: require("./modifyIntro"),
+    // signUp: require("./signUp"),
+    signIn: require("./signIn"),
+    // getInfo: require("./getInfo"),
+    // getAuth: require("./getAuth"),
+};
