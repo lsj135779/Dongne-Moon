fd0de681da89439182c34d0763582cdd430e9bc3
이승준 <lsj135779@gmail.com>
2021-11-13 14:17:50 +0900

    server:getInfo:add:use verify token

--- server/controllers/user/signUp.js
+++ server/controllers/user/signUp.js
@@ -1,34 +1,33 @@
 const { user } = require("../../models");
-const CryptoJS = require("crypto-js")
+const CryptoJS = require("crypto-js");
 
 module.exports = async (req, res) => {
+  const userInfo = await user.findOne({
+    where: {
+      email: req.body.email,
+    },
+  });
 
-    const userInfo = await user.findOne({
-        where: {
-            email: req.body.email
-        }
-    })
+  if (userInfo) {
+    return res.status(409).json({ message: "email exists" });
+  }
+  const { email, address, password } = req.body;
+  const saltIssue = CryptoJS.lib.WordArray.random(128 / 8);
+  const salt = saltIssue.toString(CryptoJS.enc.Base64);
+  const encrypted = CryptoJS.PBKDF2(password, salt, {
+    keySize: 512 / 32,
+    iterations: 1000,
+  });
+  const encryptedPW = encrypted.toString(CryptoJS.enc.Base64);
 
-    if (userInfo) {
-        res.status(409).json({ message: "email exists" });
-    }
-    const { email, address, password } = req.body;
-    const saltIssue = CryptoJS.lib.WordArray.random(128 / 8);
-    const salt = saltIssue.toString(CryptoJS.enc.Base64)
-    const encrypted = CryptoJS.PBKDF2(password, salt, {
-        keySize: 512 / 32,
-        iterations: 1000
-    });
-    const encryptedPW = encrypted.toString(CryptoJS.enc.Base64);
-
-    user.create({
-        email,
-        address,
-        password: encryptedPW,
-        salt,
-        img: "123",
-        intro: "부자",
-        nickname: "개부자"
-    })
-    res.status(200).json({ salt: salt, message: "ok" });
-}
+  user.create({
+    email,
+    address,
+    password: encryptedPW,
+    salt,
+    img: "123",
+    intro: "부자",
+    nickname: "개부자",
+  });
+  return res.status(200).json({ message: "ok" });
+};
