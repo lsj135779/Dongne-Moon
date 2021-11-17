0347e8942286ac11241b37245584958a5108d61a
rmfhsep <rmfhsep@gmail.com>
2021-11-17 18:57:19 +0900

    merge

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 
 import store from "./store/store";
 import { Provider } from "react-redux";
-
+require("dotenv").config();
 ReactDOM.render(
   <Provider store={store}>
     <App />
